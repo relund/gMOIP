7ef3d323a80f86fc646e1bfa0d52a88b39bf856a
relund <junk@relund.dk>
2019-05-22 12:49:27 +0200

    Updated one obj, two variables examples

--- inst/examples/examples.R
+++ inst/examples/examples.R
@@ -1,31 +1,239 @@
 # Define the LP max/min coeff*x st. Ax<=b, x>=0
-A <- matrix(c(9,10,2,4,-3,2), ncol = 2, byrow = TRUE)
-b <- c(90,27,3)
+A <- matrix(c(-3,2,2,4,9,10), ncol = 2, byrow = TRUE)
+b <- c(3,27,90)
 coeff <- c(7.75, 10)
 
-## LP model
-# Corner points of the polytope
-cPoints<-cornerPoints(A, b)
-plotPolytope(cPoints)
-plotPolytope(cPoints, points = cPoints)
-cPointsWLbl <- cPoints
-cPointsWLbl$lbl <- paste0("(", cPoints$x1, ",", cPoints$x2, ")")
-plotPolytope(cPointsWLbl, showLbl = TRUE)
-plotPolytope(cPoints, cPointsWLbl, showLbl = TRUE)
-plotPolytope(cPoints, cPoints, iso = coeff, crit = "max")
-plotPolytope(cPoints, cPoints, iso = c(3,-3), crit = "min")
-plotPolytope(cPoints, cPoints) + xlab("x") + ylab("y")
 
-## IP model
-# Integer points in the polytope
-iPoints<-integerPoints(A, b)
-plotPolytope(cPoints, points = iPoints)
-plotPolytope(cPoints, iPoints, iso = coeff, crit = "max", feasible = iPoints)
-plotPolytope(cPoints, iPoints, iso = c(3,-3), crit = "min", feasible = iPoints)
-cPoints<-cornerPoints(A, b, x1="int", x2="int")
-plotPolytope(cPoints, points = iPoints)
-plotPolytope(cPoints, iPoints, iso = coeff, crit = "max", feasible = iPoints)
-plotPolytope(cPoints, iPoints, iso = c(3,-3), crit = "min", feasible = iPoints)
+## LP model
+# The polytope with the corner points
+plotPolytope(
+   A,
+   b,
+   coeff,
+   type = rep("c", ncol(A)),
+   crit = "max",
+   faces = rep("c", ncol(A)),
+   plotFaces = T,
+   plotFeasible = T,
+   plotOptimum = F,
+   labels = NULL
+)
+# With optimum and labels:
+plotPolytope(
+   A,
+   b,
+   coeff,
+   type = rep("c", ncol(A)),
+   crit = "max",
+   faces = rep("c", ncol(A)),
+   plotFaces = T,
+   plotFeasible = T,
+   plotOptimum = T,
+   labels = "coord"
+)
+# Minimize:
+plotPolytope(
+   A,
+   b,
+   coeff,
+   type = rep("c", ncol(A)),
+   crit = "min",
+   faces = rep("c", ncol(A)),
+   plotFaces = T,
+   plotFeasible = T,
+   plotOptimum = T,
+   labels = "n"
+)
+# Note return a ggplot so can e.g. add other labels on e.g. the axes:
+p <- plotPolytope(
+   A,
+   b,
+   coeff,
+   type = rep("c", ncol(A)),
+   crit = "max",
+   faces = rep("c", ncol(A)),
+   plotFaces = T,
+   plotFeasible = T,
+   plotOptimum = T,
+   labels = "coord"
+)
+p + xlab("x") + ylab("y")
+
+
+## LP-model with no non-negativity constraints
+A <- matrix(c(-3, 2, 2, 4, 9, 10, 1, -2), ncol = 2, byrow = TRUE)
+b <- c(3, 27, 90, 2)
+coeff <- c(7.75, 10)
+plotPolytope(
+   A,
+   b,
+   coeff,
+   type = rep("c", ncol(A)),
+   nonneg = rep(F, ncol(A)),
+   crit = "max",
+   faces = rep("c", ncol(A)),
+   plotFaces = T,
+   plotFeasible = T,
+   plotOptimum = F,
+   labels = NULL
+)
+
+
+
+## The package don't plot feasible regions that are unbounded e.g if we drop the 2 and 3 constraint
+A <- matrix(c(-3,2), ncol = 2, byrow = TRUE)
+b <- c(3)
+coeff <- c(7.75, 10)
+
+
+plotPolytope(
+   A,
+   b,
+   coeff,
+   type = rep("c", ncol(A)),
+   crit = "max",
+   faces = rep("c", ncol(A)),
+   plotFaces = T,
+   plotFeasible = T,
+   plotOptimum = F,
+   labels = NULL
+)
+
+# One solution is to add a bounding box and check if the bounding box is binding
+A <- rbind(A, c(1,0), c(0,1))
+b <- c(b, 10, 10)
+
+plotPolytope(
+   A,
+   b,
+   coeff,
+   type = rep("c", ncol(A)),
+   crit = "max",
+   faces = rep("c", ncol(A)),
+   plotFaces = T,
+   plotFeasible = T,
+   plotOptimum = F,
+   labels = NULL
+)
+
+
+
+
+
+
+# # Load lpSolve
+# require(lpSolve)
+# A <- matrix(c(-3,2), ncol = 2, byrow = TRUE)
+# b <- c(3)
+# coeff <- c(7.75, 10)
+#
+#
+# # Direction of the constraints
+# constranints_direction  <- c("<=")
+#
+# # Find the optimal solution
+# optimum <-  lp(direction="max",
+#                objective.in = coeff,
+#                const.mat = A,
+#                const.dir = constranints_direction,
+#                const.rhs = b)
+#
+# # Print status: 0 = success, 2 = no feasible solution, 3 = unbounded (http://lpsolve.sourceforge.net/5.5/solve.htm)
+# print(optimum$status)
+#
+# # Display the optimum values for x_4p, x_3p and x_w
+# best_sol <- optimum$solution
+# names(best_sol) <- c("x_4p", "x_3p", "x_w")
+# print(best_sol)
+#
+# # Check the value of objective function at optimal point
+# print(paste("Total cost: ", optimum$objval, sep=""))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### ILP model
+A <- matrix(c(-3,2,2,4,9,10), ncol = 2, byrow = TRUE)
+b <- c(3,27,90)
+coeff <- c(7.75, 10)
+# ILP model with LP faces:
+plotPolytope(
+   A,
+   b,
+   coeff,
+   type = rep("i", ncol(A)),
+   crit = "max",
+   faces = rep("c", ncol(A)),
+   plotFaces = T,
+   plotFeasible = T,
+   plotOptimum = T,
+   labels = "coord"
+)
+#ILP model with IP faces:
+plotPolytope(
+   A,
+   b,
+   coeff,
+   type = rep("i", ncol(A)),
+   crit = "max",
+   faces = rep("i", ncol(A)),
+   plotFaces = T,
+   plotFeasible = T,
+   plotOptimum = T,
+   labels = "coord"
+)
+
+
+## MILP model
+A <- matrix(c(-3,2,2,4,9,10), ncol = 2, byrow = TRUE)
+b <- c(3,27,90)
+coeff <- c(7.75, 10)
+# Second coordinate integer
+plotPolytope(
+   A,
+   b,
+   coeff,
+   type = c("c", "i"),
+   crit = "max",
+   faces = c("c", "i"),
+   plotFaces = F,
+   plotFeasible = T,
+   plotOptimum = T,
+   labels = "coord"
+)
+# First coordinate integer and with faces:
+plotPolytope(
+   A,
+   b,
+   coeff,
+   type = c("i", "c"),
+   crit = "max",
+   faces = c("i", "c"),
+   plotFaces = T,
+   plotFeasible = T,
+   plotOptimum = T,
+   labels = "coord"
+)
+
+
+
+
+
+
+
+
 
 ## MILP model
 cPoints<-cornerPoints(A, b)
@@ -74,6 +282,11 @@ plotCriterion(zPoints, addHull = TRUE, addTriangles = TRUE, crit = "max", showLb
 zPoints<-criterionPoints(iPoints, c1 = c(-2, -1), c2 = c(1, 4), crit = "min")
 plotCriterion(zPoints, addHull = TRUE, addTriangles = TRUE, crit = "min", showLbl = TRUE)
 
+
+
+grid.arrange(p1, p2, nrow = 1)
+
+
 ## MILP model
 # x1 integer
 cPoints<-cornerPoints(A, b, x1="int", x2="cont")
@@ -96,6 +309,25 @@ plotCriterion(zRngPoints, rangePoints = zRngPoints, addTriangles = TRUE)
 zRngPoints<-criterionPoints(rngPoints, c1 = c(coeff[1], -10), c2 = c(-5, coeff[2]), crit = "min")
 plotCriterion(zRngPoints, rangePoints = zRngPoints, addTriangles = TRUE, crit = "min")
 
+# x2 integer (one plot)
+layout<-theme(plot.title = element_text(size = 5, face = "bold"))
+cPoints<-cornerPoints(A, b, x1="cont", x2="int")
+rngPoints <- ranges(A, b, x1="cont", x2="int")
+p1<-plotPolytope(cPoints, points = rngPoints, rangePoints = rngPoints) + ggtitle("Decision space") + layout
+zRngPoints<-criterionPoints(rngPoints, c1 = c(coeff[1], 0), c2 = c(0, coeff[2]), crit = "max")
+p2<-plotCriterion(zRngPoints, rangePoints = zRngPoints, addTriangles = TRUE) +
+   ggtitle(paste0("max c1 = (",coeff[1],",0), c2 = (0,",coeff[2],")")) + layout
+zRngPoints<-criterionPoints(rngPoints, c1 = c(coeff[1], -10), c2 = c(-5, coeff[2]), crit = "max")
+p3<-plotCriterion(zRngPoints, rangePoints = zRngPoints, addTriangles = TRUE) +
+   ggtitle(paste0("max c1 = (",coeff[1],",-10), c2 = (-5,",coeff[2],")")) + layout
+zRngPoints<-criterionPoints(rngPoints, c1 = c(coeff[1], -10), c2 = c(-5, coeff[2]), crit = "min")
+p4<-plotCriterion(zRngPoints, rangePoints = zRngPoints, addTriangles = TRUE, crit = "min") +
+   ggtitle(paste0("min c1 = (",coeff[1],",-10), c2 = (-5,",coeff[2],")")) + layout
+library(gridExtra)
+
+grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+}
 
 \dontrun{
 # Generate tikz file for LaTeX
