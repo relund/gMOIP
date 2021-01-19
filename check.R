ed48799db913ef1a861656c6ebd95f52c8df6e4d
Lars Relund Nielsen <junk@relund.dk>
2021-01-19 09:35:34 +0100

    Script for checking

---
+++ check.R
@@ -0,0 +1,10 @@
+## Check package
+devtools::spell_check()
+devtools::check(run_dont_test = TRUE)
+# devtools::check_rhub(platforms = c("windows-x86_64-release"), interactive = F) # is down
+devtools::check_win_release(quiet = TRUE)
+# Push files to GitHub for GitHub actions check
+
+
+# Submit to CRAN
+#devtools::release()
