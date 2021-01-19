ee96d381eedb5248cf8599efef47f9403e5ca8e3
Lars Relund Nielsen <junk@relund.dk>
2021-01-19 09:43:29 +0100

    Trim

--- check.R
+++ check.R
@@ -1,8 +1,8 @@
 ## Check package
-devtools::spell_check()
+# devtools::spell_check()
 devtools::check(run_dont_test = TRUE)
 # devtools::check_rhub(platforms = c("windows-x86_64-release"), interactive = F) # is down
-devtools::check_win_release(quiet = TRUE)
+# devtools::check_win_release(quiet = TRUE)
 # Push files to GitHub for GitHub actions check
 
 
