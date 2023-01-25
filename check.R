c79f015161882f2e933826f8d7710c5f9fe833d0
Lars Relund <junk@relund.dk>
2023-01-25 09:25:48 +0100

    New check yml

--- check.R
+++ check.R
@@ -1,7 +1,8 @@
 ## Check package
+update.packages()
 devtools::spell_check()
 spelling::update_wordlist()
-Yesdevtools::check(env_vars = c(NOT_CRAN = "true", RGL_USE_NULL = "true"))
+devtools::check(env_vars = c(NOT_CRAN = "true", RGL_USE_NULL = "true"))
 devtools::check_rhub(platforms = c("windows-x86_64-release"), interactive = F) # is down
 devtools::check_win_release(quiet = TRUE)
 # Push files to GitHub for GitHub actions check
