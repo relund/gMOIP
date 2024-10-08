644271d70733c175017776fb2ee9534e1886d72c
Lars Relund <junk@relund.dk>
2024-10-08 14:33:23 +0200

    Fix rhub

--- check.R
+++ check.R
@@ -4,7 +4,7 @@ devtools::spell_check()
 spelling::update_wordlist()
 devtools::check(env_vars = c(NOT_CRAN = "true", RGL_USE_NULL = "true"))
 # revdepcheck::revdep_check(num_workers = 4)
-devtools::check_rhub(platforms = c("windows-x86_64-release"), interactive = F)
+rhub::rhub_check(platforms = c("windows"))
 devtools::check_win_release(quiet = TRUE) # win-builder
 devtools::check_win_devel(quiet = TRUE) # win-builder
 # Push files to GitHub for GitHub actions check
